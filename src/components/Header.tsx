b3b05dd4d8c2c2d53f954d022be3fe9db4bc5f17
kent <kent@example.com>
2024-04-17 18:25:33 +0900

    final commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,11 +31,11 @@ const Header = () => {
             }}/>
             <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto justify-content-end w-100 '>
-                <Nav.Link href={APP_NAME + "/"} className='navigation-link text-black bg-transparent fw-bold'>home</Nav.Link>
-                <Nav.Link href={APP_NAME + "/gallery"} className='navigation-link text-black bg-transparent fw-bold'>gallery</Nav.Link>
-                <Nav.Link href={APP_NAME + "/events"} className='navigation-link text-black bg-transparent fw-bold'>events</Nav.Link>
-                <Nav.Link href={APP_NAME + "/shinkan"} className='navigation-link text-black bg-transparent fw-bold'>shinkan</Nav.Link>
-                <Nav.Link href={APP_NAME + "/contacts"} className='navigation-link text-black bg-transparent fw-bold'>contacts</Nav.Link>
+                <Nav.Link href="/" className='navigation-link text-black bg-transparent fw-bold'>home</Nav.Link>
+                <Nav.Link href="/gallery" className='navigation-link text-black bg-transparent fw-bold'>gallery</Nav.Link>
+                <Nav.Link href= "/events" className='navigation-link text-black bg-transparent fw-bold'>events</Nav.Link>
+                <Nav.Link href="/shinkan" className='navigation-link text-black bg-transparent fw-bold'>shinkan</Nav.Link>
+                <Nav.Link href="/contacts" className='navigation-link text-black bg-transparent fw-bold'>contacts</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
